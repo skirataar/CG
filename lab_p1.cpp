64f02e23096fe765476d102809d488facb846a9e
skirataar <95690923+skirataar@users.noreply.github.com>
2024-08-05 10:39:18 +0530

    Update lab_p1.cpp

--- lab_p1.cpp
+++ lab_p1.cpp
@@ -1,58 +1,62 @@
-#include<math.h>
-#include<GL/glut.h>
+#include <cmath>
+#include <GL/glut.h>
 
-void Bresen(int x1, int y1, int x2, int y2) {
-    glPointSize(20);
+void setPixel(int x, int y) {
     glBegin(GL_POINTS);
-    glColor3f(1, 1, 1);
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-    int p = 2 * dy - dx;
-    int inc = 2 * dy;
-    int dec = 2 * dy - 2 * dx;
-    int x, y, xend;
-    if (y1 < y2) {
-        x = x1;
-        y = y1;
-        xend = x2;
-    }
-    else {
-        x = x2;
-        y = y2;
-        xend = x1;
-    }
     glVertex2i(x, y);
-    while (x < xend) {
-        x += 1;
-        if (p >= 0) {
-            p += dec;
-            y += 1;
-        }
-        else {
-            p += inc;
-        }
-        glVertex2i(x, y);
-    }
     glEnd();
+}
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void lineBres(int x0, int y0, int xEnd, int yEnd) {
+    int dx = std::abs(xEnd - x0), dy = std::abs(yEnd - y0);
+    int p = 2 * dy - dx;
+    int twoDy = 2 * dy, twoDyMinusDx = 2 * (dy - dx);
+    int x = x0, y = y0;
+
+    if (x0 > xEnd) {
+        swap(x0, xEnd);
+        swap(y0, yEnd);
+    }
+
+    setPixel(x, y);
+
+    while (x < xEnd) {
+        x++;
+        if (p < 0)
+            p += twoDy;
+        else {
+            y++;
+            p += twoDyMinusDx;
+        }
+        setPixel(x, y);
+    }
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0); // White color
+    lineBres(50, 100, 200, 300);
     glFlush();
 }
 
-void disp2() {
-    glClearColor(0, 0, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-    Bresen(-9, -1, 9, 3);
+void init() {
+    gluOrtho2D(0.0, 500.0, 0.0, 500.0);
 }
-
-
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(10, 10);
-    glutInitWindowSize(400, 400);
-    glutCreateWindow("Bresen");
-    gluOrtho2D(-10, 10, -10, 10);
-    glutDisplayFunc(disp2);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Bresenham Line Drawing");
+    init();
+    glutDisplayFunc(display);
     glutMainLoop();
     return 0;
 }
